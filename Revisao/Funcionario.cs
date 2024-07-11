4db942920c23ac9c65b03867e7692c6e4d493db3
Gabriel Augusto <gabriel.cursoaugustus@gmail.com>
2024-07-11 16:15:22 -0300

    Adicionar arquivos de projeto.

---
+++ Revisao/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao
+{
+    public class Funcionario:Pessoa
+    {
+        public int matricula { get; set; }
+        private string placaVeiculo { get; set; }
+
+        public bool verificarVeiculo(Automovel automovel)
+        {
+            return true;
+        }
+    }
+}
